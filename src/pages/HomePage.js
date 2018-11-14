322ffd96a0263f35d67f3c5b5b0bddb83966d6f1
echeng <wangyan6783@gmail.com>
2018-11-14 18:00:06 -0500

    homepage styling

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,21 +6,17 @@ import homepageOverlay from '../images/homepage-overlay.png';
 const HomePage = () => {
   return (
     <Fragment>
-      <div className="homepage">
         <div className="homepage-video">
           <video loop autoPlay>
              <source src={homepageVideo} type="video/mp4" />
           </video>
-          <img className="homevideo-overlay" src={homepageOverlay} alt="snow hub homepage" />
         </div>
-     </div>
+        <div className="homepage-overlay">
+          <img src={homepageOverlay} alt="snow hub homepage" />
+        </div>
+
     </Fragment>
   )
 }
 
 export default HomePage
-
-
-  // <img src="https://www.rei.com/content/dam/images/Expert%20Advice/Migration/HeroImages/Content_Team_082817_18850_Snowboards_Choose_lg.jpg" alt="" height="550px" width="960px" />
-
-    // <video src={homepageVideo} />
